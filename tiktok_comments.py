3b8aee58bc45ac4f97d085fd906ea2747db5a38d
Kim Tran <kdt46@case.edu>
2023-10-26 19:41:37 -0400

    tiktok target

--- tiktok_comments.py
+++ tiktok_comments.py
@@ -2,7 +2,9 @@ from apify_client import ApifyClient
 import json
 
 # Initialize the ApifyClient with your API token
-client = ApifyClient("apify_api_KEjtWbDL1d7Bx0wRSGHuHJpqEmJAyE1Ixnbe")
+# client = ApifyClient("apify_api_KEjtWbDL1d7Bx0wRSGHuHJpqEmJAyE1Ixnbe")
+client = ApifyClient("apify_api_uSIyHxzUx667snZJ2mMDrSEU0Cxk6E0dmXHd")
+
 
 posts = {}
 list_of_urls = []
@@ -12,15 +14,16 @@ run_posts_input = {
         "profiles": ["target"]
 }
 
-# posts_run = client.actor("clockworks/tiktok-scraper").call(run_input=run_posts_input)
+posts_run = client.actor("clockworks/tiktok-scraper").call(run_input=run_posts_input)
 
-"""for item in client.dataset(posts_run["defaultDatasetId"]).iterate_items():
+for item in client.dataset(posts_run["defaultDatasetId"]).iterate_items():
     url = item["webVideoUrl"]
     posts[url] = {}
     posts[url]["timestamp"] = item["createTimeISO"]
     posts[url]["comments"] = []
-    list_of_urls.append(url)"""
-with open("data/tiktok_target_posts.json") as data_file:
+    list_of_urls.append(url)
+
+with open("data/tiktok_nike_posts.json") as data_file:
     data = json.load(data_file)
     url_field = "webVideoUrl"
     for item in data:
@@ -37,7 +40,7 @@ run_comments_input = {
 comments_run = client.actor("clockworks/tiktok-comments-scraper").call(run_input=run_comments_input)
 
 for comment in client.dataset(comments_run["defaultDatasetId"]).iterate_items():
-     posts[comment["videoWebUrl"]]["comments"].append(comment["text"])
+    posts[comment["videoWebUrl"]]["comments"].append(comment["text"])
 
 print(posts)
 data = json.dumps(posts, indent=4)
